778dd83bbf8bc4cc4ab328a1883313ee77770ad1
Alexnader Rahov <techrahovski@gmail.com>
2020-11-26 12:47:14 +0300

    v3.0 - pre release Need to fix register with digits(getReg)

--- Asm_simple_kata/Source.cpp
+++ Asm_simple_kata/Source.cpp
@@ -22,6 +22,7 @@ string parse_command(string command, unsigned int& index_in_str) {
 	return result;
 }
 
+//how to avoid names with digits
 static int& getReg(unordered_map<string, int>& regs, string name) {
 	return regs[name];
 }
@@ -49,7 +50,15 @@ void execute_command(unordered_map<string, int>& regs, string command, string re
 
 unordered_map<string, int> assembler(const vector<string>& program) {
 	unordered_map<string, int> reg_info;
+
+	//flag and system register for jnz
+	unsigned int jump_register = 0;//if command jnz 5 -2
+	bool isJnzDone = false;
+
 	for (unsigned int i = 0; i < program.size(); i++) {
+		if (i == 6) {
+			cout << "KEK" << endl;
+		}
 		unsigned int index = 0;
 		string command = parse_command(program[i], index);
 		if (command != "jnz") {
@@ -59,18 +68,33 @@ unordered_map<string, int> assembler(const vector<string>& program) {
 		}
 		else {
 			string reg = parse_command(program[i], index);
-			//need to work with
-			//jnz b - 2 (+++)
-			//jnz 5 10 (---)
+			//check, if we have jnz command - jnz (digit) -2
+			if (isdigit(reg[0])) {
+				if (!jump_register) {
+					//if we completed jnz command, when we will go to next command
+					if (isJnzDone) {
+						isJnzDone = false;
+						continue;
+					}
+					else {
+						jump_register = stoi(reg);
+					}
+				}
+			}
 			string str_value = parse_command(program[i], index);
 			int value = stoi(str_value);
-			//
-			index = 0;
 			if ((i + value < 0) || (i + value >= program.size())) {
 				continue;
 			}
-			if (getReg(reg_info, reg)) {
+			if (jump_register) {
 				i = i + value - 1;
+				jump_register--;
+				if (!jump_register) {
+					isJnzDone = true;
+				}
+			} else  if (getReg(reg_info, reg)) {
+				i = i + value - 1;
+
 			}
 		}
 	}
@@ -90,3 +114,37 @@ int main() {
 	unordered_map<string, int> answer = assembler(asm_command);
 	return 0;
 }
+
+/*
+8
+mov d 100
+dec d
+mov b d
+jnz b -2
+inc d
+mov a d
+jnz 5 10
+mov c a
+
+
+equal to [ [unsupported type], [unsupported type], [unsupported type] ]
+Actual: [ [unsupported type], [unsupported type], [unsupported type], [unsupported type] ]
+*/
+
+/*
+11
+mov c 12
+mov b 0
+mov a 200
+dec a
+inc b
+jnz a -2
+dec c
+mov a b
+jnz c -5
+jnz 0 1
+mov c a
+
+Expected: equal to [ [unsupported type], [unsupported type], [unsupported type] ]
+Actual: [ [unsupported type], [unsupported type], [unsupported type], [unsupported type] ]
+*/
