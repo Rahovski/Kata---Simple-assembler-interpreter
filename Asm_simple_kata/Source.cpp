c0dd358f95c9f8b1c577af7d48b150bd24d451ed
Alexnader Rahov <techrahovski@gmail.com>
2020-12-01 13:00:51 +0300

    Refactor 1 - Извлечение метода jnz

--- Asm_simple_kata/Source.cpp
+++ Asm_simple_kata/Source.cpp
@@ -6,6 +6,43 @@
 #include <unordered_map>
 using namespace std;
 
+string parse_command(string command, unsigned int& index_in_str);
+static int& getReg(unordered_map<string, int>& regs, string name);
+void execute_command(unordered_map<string, int>& regs, string command, string reg, string val);
+unordered_map<string, int> assembler(const vector<string>& program);
+void execute_jnz_command(string reg_name, unsigned int& jmp_reg, bool& jnzSuccess, unsigned int& num_of_command, unordered_map<string, int>& regs, int com_value);
+
+unordered_map<string, int> assembler(const vector<string>& program) {
+	unordered_map<string, int> reg_info;
+
+	//flag and system register for jnz
+	unsigned int jump_register = 0;//if command jnz 5 -2
+	bool isJnzDone = false;
+
+	for (unsigned int i = 0; i < program.size(); i++) {
+		unsigned int index = 0;
+		string command = parse_command(program[i], index);
+		string reg = parse_command(program[i], index);
+		string str_value = parse_command(program[i], index);
+		if (command != "jnz") {
+			execute_command(reg_info, command, reg, str_value);
+		}
+		else {
+			int value = stoi(str_value);//steps for jump - int
+			if ((i + value < 0)) {
+				i = -1;
+				continue;
+			}
+			else if (i + value >= program.size()){
+				i = program.size();
+				continue;
+			}
+			execute_jnz_command(reg, jump_register, isJnzDone, i, reg_info, value);
+		}
+	}
+	return reg_info;
+}
+
 string parse_command(string command, unsigned int& index_in_str) {
 	string result = "";
 	while (index_in_str < command.length()) {
@@ -48,59 +85,29 @@ void execute_command(unordered_map<string, int>& regs, string command, string re
 	}
 }
 
-unordered_map<string, int> assembler(const vector<string>& program) {
-	unordered_map<string, int> reg_info;
-
-	//flag and system register for jnz
-	unsigned int jump_register = 0;//if command jnz 5 -2
-	bool isJnzDone = false;
-
-	for (unsigned int i = 0; i < program.size(); i++) {
-		unsigned int index = 0;
-		string command = parse_command(program[i], index);
-		if (command != "jnz") {
-			string reg = parse_command(program[i], index);
-			string str_value = parse_command(program[i], index);
-			execute_command(reg_info, command, reg, str_value);
+void execute_jnz_command(string reg_name, unsigned int& jmp_reg, bool& jnzSuccess, unsigned int& num_of_command, unordered_map<string, int>& regs, int com_value) {
+	//check, if we have jnz command - jnz (digit) -2
+	if (isdigit(reg_name[0])) {
+		if (!jmp_reg) {
+			//if we completed jnz command, when we will go to next command
+			if (jnzSuccess) {
+				jnzSuccess = false;
+			}
+			else {
+				jmp_reg = stoi(reg_name);
+			}
 		}
-		else {
-			string reg = parse_command(program[i], index);
-			string str_value = parse_command(program[i], index);//steps for jump - string
-			int value = stoi(str_value);//steps for jump - int
-			if ((i + value < 0)) {
-				i = -1;
-				continue;
-			}
-			else if (i + value >= program.size()){
-				i = program.size();
-				continue;
-			}
-			//check, if we have jnz command - jnz (digit) -2
-			if (isdigit(reg[0])) {
-				if (!jump_register) {
-					//if we completed jnz command, when we will go to next command
-					if (isJnzDone) {
-						isJnzDone = false;
-						continue;
-					}
-					else {
-						jump_register = stoi(reg);
-					}
-				}
-				if (jump_register) {
-					i = i + value - 1;
-					jump_register--;
-					if (!jump_register) {
-						isJnzDone = true;
-					}
-				}
-			} else  if (getReg(reg_info, reg)) {
-				i = i + value - 1;
-
+		if (jmp_reg) {
+			num_of_command = num_of_command + com_value - 1;
+			jmp_reg--;
+			if (!jmp_reg) {
+				jnzSuccess = true;
 			}
 		}
 	}
-	return reg_info;
+	else  if (getReg(regs, reg_name)) {
+		num_of_command = num_of_command + com_value - 1;
+	}
 }
 
 int main() {
