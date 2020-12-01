1dbdf325adcd0cc595c365b54d8fec6e992de658
Alexnader Rahov <techrahovski@gmail.com>
2020-12-01 13:55:56 +0300

    First skelet of OOP solution

--- Asm_simple_kata/Source.cpp
+++ Asm_simple_kata/Source.cpp
@@ -1,113 +1,102 @@
 /* https://www.codewars.com/kata/58e24788e24ddee28e000053/train/cpp  */
 
+//OOP solution
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 #include <unordered_map>
 using namespace std;
-
-string parse_command(string command, unsigned int& index_in_str);
 static int& getReg(unordered_map<string, int>& regs, string name);
-void execute_command(unordered_map<string, int>& regs, string command, string reg, string val);
-unordered_map<string, int> assembler(const vector<string>& program);
-void execute_jnz_command(string reg_name, unsigned int& jmp_reg, bool& jnzSuccess, unsigned int& num_of_command, unordered_map<string, int>& regs, int com_value);
+vector<string> split(string s);
+
+class Command {
+protected:
+	string action_register;
+	string com_argument;
+public:
+	virtual void command_execute(unordered_map<string, int>& regs_memory) = 0;
+};
+
+class mov_command : public Command {
+public:
+	mov_command(string reg, string arg) {
+		action_register = reg;
+		com_argument = arg;
+	}
+	void command_execute(unordered_map<string, int>& regs_memory) {
+		getReg(regs_memory, action_register)--;
+	}
+};
+
+class inc_command : public Command {
+public:
+	inc_command(string reg) {
+		action_register = reg;
+	}
+	void command_execute(unordered_map<string, int>& regs_memory) {
+		getReg(regs_memory, action_register)++;
+	}
+};
+
+class dec_command : public Command {
+public:
+	dec_command(string reg) {
+		action_register = reg;
+	}
+	void command_execute(unordered_map<string, int>& regs_memory) {
+		getReg(regs_memory, action_register)--;
+	}
+};
+
+class jnz_command : public Command {
+public:
+	jnz_command(string reg, string arg) {
+		action_register = reg;
+		com_argument = arg;
+	}
+	void command_execute(unordered_map<string, int>& regs_memory) {
+		getReg(regs_memory, action_register)--;
+	}
+};
 
 unordered_map<string, int> assembler(const vector<string>& program) {
 	unordered_map<string, int> reg_info;
-
-	//flag and system register for jnz
-	unsigned int jump_register = 0;//if command jnz 5 -2
-	bool isJnzDone = false;
-
+	vector<Command*> command_stack(program.size());
 	for (unsigned int i = 0; i < program.size(); i++) {
-		unsigned int index = 0;
-		string command = parse_command(program[i], index);
-		string reg = parse_command(program[i], index);
-		string str_value = parse_command(program[i], index);
-		if (command != "jnz") {
-			execute_command(reg_info, command, reg, str_value);
+		vector<string> com_parts = split(program[i]);
+		if (com_parts[0] == "mov") {
+			command_stack[i] = new mov_command(com_parts[1], com_parts[2]);
 		}
-		else {
-			int value = stoi(str_value);//steps for jump - int
-			if ((i + value < 0)) {
-				i = -1;
-				continue;
-			}
-			else if (i + value >= program.size()){
-				i = program.size();
-				continue;
-			}
-			execute_jnz_command(reg, jump_register, isJnzDone, i, reg_info, value);
+		else if (com_parts[0] == "inc") {
+			command_stack[i] = new inc_command(com_parts[1]);
 		}
+		else if (com_parts[0] == "dec") {
+			command_stack[i] = new dec_command(com_parts[1]);
+		}
+		else if (com_parts[0] == "jnz") {
+			command_stack[i] = new jnz_command(com_parts[1], com_parts[2]);
+		}
+	}
+	//free memory from object
+	for (unsigned int i = 0; i < command_stack.size(); i++) {
+		delete command_stack[i];
 	}
 	return reg_info;
 }
 
-string parse_command(string command, unsigned int& index_in_str) {
-	string result = "";
-	while (index_in_str < command.length()) {
-		if ((command[index_in_str] != ' ')) {
-			result += command[index_in_str];
-			index_in_str++;
-		}
-		else {
-			index_in_str++;
-			return result;
-		}
-	}
-	index_in_str++;
-	return result;
-}
-
-//how to avoid names with digits
 static int& getReg(unordered_map<string, int>& regs, string name) {
 	return regs[name];
 }
 
-void execute_command(unordered_map<string, int>& regs, string command, string reg, string val = "") {
-	if (command == "mov") {
-		if (isdigit(val[0]) || val[0] == '-') {
-			int value = stoi(val);
-			//regs[reg] = value;
-			getReg(regs, reg) = value;
-		}
-		else {
-			getReg(regs, reg) = getReg(regs, val);
-		}
+vector<string> split(string s) {
+	istringstream str(s);
+	string item;
+	vector<string> part_command;
+	while (getline(str, item, ' ')) {
+		part_command.push_back(item);
 	}
-	else if (command == "inc") {
-		//regs[reg]++;
-		getReg(regs, reg)++;
-	}
-	else if (command == "dec") {
-		//regs[reg]++;
-		getReg(regs, reg)--;
-	}
-}
-
-void execute_jnz_command(string reg_name, unsigned int& jmp_reg, bool& jnzSuccess, unsigned int& num_of_command, unordered_map<string, int>& regs, int com_value) {
-	//check, if we have jnz command - jnz (digit) -2
-	if (isdigit(reg_name[0])) {
-		if (!jmp_reg) {
-			//if we completed jnz command, when we will go to next command
-			if (jnzSuccess) {
-				jnzSuccess = false;
-			}
-			else {
-				jmp_reg = stoi(reg_name);
-			}
-		}
-		if (jmp_reg) {
-			num_of_command = num_of_command + com_value - 1;
-			jmp_reg--;
-			if (!jmp_reg) {
-				jnzSuccess = true;
-			}
-		}
-	}
-	else  if (getReg(regs, reg_name)) {
-		num_of_command = num_of_command + com_value - 1;
-	}
+	return part_command;
 }
 
 int main() {
